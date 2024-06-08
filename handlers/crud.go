1684609c0356f29ec32f14e5f398ad7e8fee74e1
aamir290895 <aamir.007sdl.ak@gmail.com>
2024-06-09 03:34:16 +0530

    ..

--- handlers/crud.go
+++ handlers/crud.go
@@ -8,6 +8,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// used mutex for manage concurrency.
+
 func CreateEmployee(c *gin.Context) {
 	var e models.Employee
 	if err := c.ShouldBindJSON(&e); err != nil {
